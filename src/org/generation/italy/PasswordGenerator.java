e994e1b71d9a725e0612f5b1844ef6c49e9ea327
GianlucaPensa <janjyboy2000@gmail.com>
2021-11-15 17:59:35 +0100

    correzione

--- src/org/generation/italy/PasswordGenerator.java
+++ src/org/generation/italy/PasswordGenerator.java
@@ -4,14 +4,14 @@ public class PasswordGenerator {
 
 	public static void main(String[] args) {
 		
-		String nome = "Pinco-";
-		String cognome ="Pallo-";
-		String colore ="magenta-";
+		String nome = "Pinco";
+		String cognome ="Pallo";
+		String colore ="magenta";
 		int mese = 5;
 		int anno = 1994;
 		int giorno = 12;
 		int data = mese + anno + giorno;
-		String password = nome + cognome + colore + data;
+		String password = nome + "-" + cognome + "-" + colore + "-"  + data;
 		
 		System.out.println(password);
 	}
